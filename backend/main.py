f53bba873e07e3093e95395a6bf1f437ab5edb3c
YorklinPunk <zetita159@gmail.com>
2025-12-06 23:07:03 -0500

    Agregando endpoint de data de ejemplo

--- backend/main.py
+++ backend/main.py
@@ -6,3 +6,7 @@ app = FastAPI(title="internal-backend")
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
+@app.get("/data")
+async def get_data():
+    return {"data": "Estos son datos públicos del backend"}
